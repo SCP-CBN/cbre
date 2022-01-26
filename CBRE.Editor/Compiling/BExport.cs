a12262b0caadeb1c9c626c7a79bb2e68315d565c
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-01-26 22:11:11 +0100

    idfk

--- CBRE.Editor/Compiling/BExport.cs
+++ CBRE.Editor/Compiling/BExport.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
+using CBRE.DataStructures.Geometric;
 using CBRE.DataStructures.MapObjects;
 using CBRE.Editor.Documents;
 
 namespace CBRE.Editor.Compiling {
     class BExport {
+        private class BVertex {
+            public BVertex(Coordinate pos, Coordinate norm, Coordinate tang, Coordinate bitang, double u, double v) {
+                this.pos = pos; this.norm = norm; this.tang = tang; this.bitang = bitang; this.u = u; this.v = v;
+            }
+            public Coordinate pos;
+            public Coordinate norm;
+            public Coordinate tang;
+            public Coordinate bitang;
+            public double u; public double v;
+        }
+
         public static void SaveToFile(string filename, Document document, ExportForm form) {
             List<string> textures = new List<string>();
             List<Dictionary<Vertex, int>> allVertToInd = new List<Dictionary<Vertex, int>>();
-            List<List<Vertex>> allVertices = new List<List<Vertex>>();
+            List<List<BVertex>> allVertices = new List<List<BVertex>>();
             List<List<int>> allIndices = new List<List<int>>();
             
             { // Gathering mesh data.
@@ -22,7 +34,7 @@ namespace CBRE.Editor.Compiling {
                         .Where(x => !x.Key.StartsWith("tooltextures/"))) {
                     textures.Add(group.Key);
                     Dictionary<Vertex, int> vertToInd = new Dictionary<Vertex, int>();
-                    List<Vertex> vertices = new List<Vertex>();
+                    List<BVertex> vertices = new List<BVertex>();
                     List<int> indices = new List<int>();
                     allVertToInd.Add(vertToInd);
                     allVertices.Add(vertices);
@@ -31,18 +43,37 @@ namespace CBRE.Editor.Compiling {
                         foreach (Vertex v in f.Vertices) {
                             bool found = false;
                             for (int i = 0; i < vertices.Count; i++) {
-                                if (vertices[i].Location.EquivalentTo(v.Location, 0.01M)
-                                    && vertices[i].Parent.Plane.Normal.EquivalentTo(v.Parent.Plane.Normal, 0.01M)
-                                    && Math.Abs(vertices[i].DTextureU - v.DTextureU) < 0.01
-                                    && Math.Abs(vertices[i].DTextureV - v.DTextureV) < 0.01) {
-                                    vertToInd.Add(v, i);
-                                    found = true;
-                                    break;
+                                if (vertices[i].pos.EquivalentTo(v.Location, 0.01M)) {
+                                    if (vertices[i].norm.EquivalentTo(v.Parent.Plane.Normal, 0.01M)
+                                    && Math.Abs(vertices[i].u - v.DTextureU) < 0.01
+                                    && Math.Abs(vertices[i].v - v.DTextureV) < 0.01) {
+                                        vertToInd.Add(v, i);
+                                        found = true;
+                                        break;
+                                    }
+                                    if ((vertices[i].norm - v.Parent.Plane.Normal).VectorMagnitude() < 1.0M) {
+                                        form.ProgressLog.Invoke((MethodInvoker)(() => form.ProgressLog.AppendText("\nBased Department!")));
+                                        vertices[i].norm = ((vertices[i].norm + v.Parent.Plane.Normal) * 0.5M).Normalise();
+                                        vertices[i].tang = ((vertices[i].tang + v.Parent.Texture.UAxis) * 0.5M).Normalise();
+                                        vertices[i].bitang = ((vertices[i].bitang + v.Parent.Texture.VAxis) * 0.5M).Normalise();
+
+                                        vertToInd.Add(v, vertices.Count);
+                                        vertices.Add(new BVertex(v.Location,
+                                            vertices[i].norm, vertices[i].tang, vertices[i].bitang,
+                                            v.DTextureU, v.DTextureV
+                                        ));
+
+                                        found = true;
+                                        break;
+                                    }
                                 }
                             }
                             if (!found) {
                                 vertToInd.Add(v, vertices.Count);
-                                vertices.Add(v);
+                                vertices.Add(new BVertex(v.Location,
+                                    v.Parent.Plane.Normal, v.Parent.Texture.UAxis, v.Parent.Texture.VAxis,
+                                    v.DTextureU, v.DTextureV)
+                                );
                             }
                         }
 
@@ -70,25 +101,25 @@ namespace CBRE.Editor.Compiling {
                 for (int i = 0; i < textures.Count; i++) {
                     br.WriteNullTerminatedString(textures[i]);
                     Dictionary<Vertex, int> vertToInd = allVertToInd[i];
-                    List<Vertex> vertices = allVertices[i];
+                    List<BVertex> vertices = allVertices[i];
                     List<int> indices = allIndices[i];
 
                     br.Write(vertices.Count);
-                    foreach (Vertex v in vertices) {
-                        br.Write((float)v.Location.DX);
-                        br.Write((float)v.Location.DZ);
-                        br.Write((float)v.Location.DY);
-                        br.Write((float)v.Parent.Plane.Normal.DX);
-                        br.Write((float)v.Parent.Plane.Normal.DZ);
-                        br.Write((float)v.Parent.Plane.Normal.DY);
-                        br.Write((float)v.Parent.Texture.UAxis.X);
-                        br.Write((float)v.Parent.Texture.UAxis.Z);
-                        br.Write((float)v.Parent.Texture.UAxis.Y);
-                        br.Write((float)v.Parent.Texture.VAxis.X);
-                        br.Write((float)v.Parent.Texture.VAxis.Z);
-                        br.Write((float)v.Parent.Texture.VAxis.Y);
-                        br.Write((float)v.DTextureU);
-                        br.Write((float)v.DTextureV);
+                    foreach (BVertex v in vertices) {
+                        br.Write((float)v.pos.DX);
+                        br.Write((float)v.pos.DZ);
+                        br.Write((float)v.pos.DY);
+                        br.Write((float)v.norm.DX);
+                        br.Write((float)v.norm.DZ);
+                        br.Write((float)v.norm.DY);
+                        br.Write((float)v.tang.DX);
+                        br.Write((float)v.tang.DZ);
+                        br.Write((float)v.tang.DY);
+                        br.Write((float)v.bitang.DX);
+                        br.Write((float)v.bitang.DZ);
+                        br.Write((float)v.bitang.DY);
+                        br.Write((float)v.u);
+                        br.Write((float)v.v);
                     }
                     br.Write(indices.Count);
                     foreach (int index in indices) {
